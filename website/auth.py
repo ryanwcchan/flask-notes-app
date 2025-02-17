e53920a20de036d7d756a3d5bb56c064e02a3411
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2025-02-16 22:24:30 -0500

    Setup flask app with login and sign up views.

---
+++ website/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, redirect, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    data = request.form
+    print(data)
+    return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+    return redirect('/')
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 1 characters.', category='error')
+        elif password != confirmPassword:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+            flash('Account created!', category='success')
+
+    return render_template("signup.html")
